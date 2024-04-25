ce33a805e051970ba52a2ca6e80000c7ed5c6a24
spiritevolution <158512066+spiritevolution@users.noreply.github.com>
2024-04-25 20:44:24 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,14 @@
+    return data
+        print(f"Random Number: {item}")
+
+
+    data = generate_random_data()
+if __name__ == "__main__":
+
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    main()
+    for item in data:
+def generate_random_data():
+import random
